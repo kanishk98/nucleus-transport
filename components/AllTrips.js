a38cb63726a2da0d59fb8c73b140ab31516883bb
kanishk98 <kanishk.kakar@gmail.com>
2018-11-14 13:23:48 +0530

    Build navigation structure

---
+++ components/AllTrips.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default class AllTrips extends React.PureComponent {
+    render() {
+        return <Text>All trips here</Text>
+    }
+}
