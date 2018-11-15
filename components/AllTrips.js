4c0488b2b6aed54ca332e3f85c68712b07334d87
kanishk98 <kanishk.kakar@gmail.com>
2018-11-15 23:07:05 +0530

    Add fetch methods and basic error handling to AllTrips

--- components/AllTrips.js
+++ components/AllTrips.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import {SectionList, FlatList} from 'react-native';
+import {SectionList, FlatList, StyleSheet} from 'react-native';
 import BusCard from './BusCard';
+import Constants from '../Constants';
+import { renderIf } from './renderIf';
 
 export default class AllTrips extends React.PureComponent {
     
     constructor(props) {
         super(props);
+        this.state = {
+            error: null,
+        }
         this.fetchInitialBuses();
     }
 
     fetchInitialBuses = () => {
-        
+        fetch(Constants.transportIp + '/get-buses')
+        .then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+            this.setState({error: err});
+        });
     }
 
     render() {
-        return <BusCard />
+        const { error } = this.state;
+        return (
+            renderIf(
+                !!error,
+                <View style={styles.container}>
+                    <BusCard />
+                </View>,
+                <View style={styles.container}>
+                    <BusCard />
+                </View>
+            )
+        );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
